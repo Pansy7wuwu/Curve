6ccd5321a9b0572e058f70a6b7ab04e5ffec4b42
Pansy7wuwu <2194002315@qq.com>
2022-08-01 07:56:47 +0800

    commit ninth

--- language_tra_cn.ts
+++ language_tra_cn.ts
@@ -9,59 +9,242 @@
         <translation>曲綫軟件</translation>
     </message>
     <message>
-        <location filename="curvefont.ui" line="39"/>
+        <location filename="curvefont.ui" line="59"/>
+        <source>All</source>
+        <translation>信息</translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="105"/>
+        <location filename="curvefont.ui" line="175"/>
+        <location filename="curvefont.ui" line="188"/>
+        <location filename="curvefont.ui" line="292"/>
+        <location filename="curvefont.ui" line="457"/>
+        <location filename="curvefont.ui" line="496"/>
+        <location filename="curvefont.ui" line="584"/>
+        <location filename="curvefont.ui" line="622"/>
+        <location filename="curvefont.ui" line="642"/>
+        <location filename="curvefont.ui" line="687"/>
+        <location filename="curvefont.ui" line="795"/>
+        <location filename="curvefont.ui" line="884"/>
+        <location filename="curvefont.ui" line="897"/>
+        <location filename="curvefont.ui" line="910"/>
+        <location filename="curvefont.ui" line="962"/>
+        <location filename="curvefont.ui" line="1090"/>
+        <location filename="curvefont.ui" line="1166"/>
+        <source>CheckBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1202"/>
+        <source>             曲线名</source>
+        <translation>             曲綫名</translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1227"/>
+        <source>                           数值</source>
+        <translation>                           數據</translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1234"/>
+        <source>       颜色</source>
+        <translation>       顔色</translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1241"/>
+        <source>   时间</source>
+        <translation>   時間</translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1262"/>
+        <source>Settings</source>
+        <translation>設置</translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1294"/>
         <source>文件</source>
         <translation>文件</translation>
     </message>
     <message>
-        <location filename="curvefont.ui" line="46"/>
+        <location filename="curvefont.ui" line="1298"/>
+        <source>历史文件</source>
+        <translation>歷史文件</translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1317"/>
         <source>语言</source>
         <translation>語言</translation>
     </message>
     <message>
-        <location filename="curvefont.ui" line="58"/>
+        <location filename="curvefont.ui" line="1329"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1344"/>
         <source>退出</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="curvefont.ui" line="61"/>
+        <location filename="curvefont.ui" line="1347"/>
         <source>Ctrl+W</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="curvefont.ui" line="66"/>
+        <location filename="curvefont.ui" line="1352"/>
         <source>打开文件</source>
         <translation>打開文件</translation>
     </message>
     <message>
-        <location filename="curvefont.ui" line="69"/>
+        <location filename="curvefont.ui" line="1355"/>
         <source>Ctrl+O</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="curvefont.ui" line="74"/>
+        <location filename="curvefont.ui" line="1360"/>
         <source>简体中文</source>
         <translation>簡體中文</translation>
     </message>
     <message>
-        <location filename="curvefont.ui" line="79"/>
+        <location filename="curvefont.ui" line="1365"/>
         <source>英文</source>
         <translation>英文</translation>
     </message>
     <message>
-        <location filename="curvefont.ui" line="84"/>
+        <location filename="curvefont.ui" line="1370"/>
         <source>繁体中文</source>
         <translation>繁體中文</translation>
     </message>
     <message>
-        <location filename="curvefont.cpp" line="55"/>
+        <location filename="curvefont.ui" line="1375"/>
+        <source>HistoryFile0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1380"/>
+        <source>HistoryFile1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1385"/>
+        <source>HistoryFile2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1390"/>
+        <source>HistoryFile3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1395"/>
+        <source>HistoryFile4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1400"/>
+        <source>HistoryFile5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1405"/>
+        <source>HistoryFile6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1410"/>
+        <source>HistoryFile7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1415"/>
+        <source>HistoryFile8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1420"/>
+        <source>HistoryFile9</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1429"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1432"/>
+        <source>OpenFile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1441"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1450"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.ui" line="1459"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.cpp" line="209"/>
+        <location filename="curvefont.cpp" line="1477"/>
+        <location filename="curvefont.cpp" line="1817"/>
+        <location filename="curvefont.cpp" line="1887"/>
+        <location filename="curvefont.cpp" line="1957"/>
+        <location filename="curvefont.cpp" line="2027"/>
+        <location filename="curvefont.cpp" line="2097"/>
+        <location filename="curvefont.cpp" line="2167"/>
+        <location filename="curvefont.cpp" line="2237"/>
+        <location filename="curvefont.cpp" line="2307"/>
+        <location filename="curvefont.cpp" line="2377"/>
+        <location filename="curvefont.cpp" line="2447"/>
+        <source>正在处理，请稍后...</source>
+        <translation>正在處理，請稍後...</translation>
+    </message>
+    <message>
+        <location filename="curvefont.cpp" line="212"/>
+        <location filename="curvefont.cpp" line="1480"/>
+        <location filename="curvefont.cpp" line="1592"/>
         <source>Open File</source>
         <translation>打開文件</translation>
     </message>
     <message>
-        <location filename="curvefont.cpp" line="56"/>
-        <source>(*.kdt *.KDT *.DAT *.dat *.SDR *.sdr)</source>
+        <location filename="curvefont.cpp" line="213"/>
+        <location filename="curvefont.cpp" line="1481"/>
+        <source>(*.kdt *.KDT *.DAT *.dat *.SDR *.sdr *.tdt *.TDT)</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="curvefont.cpp" line="274"/>
+        <location filename="curvefont.cpp" line="1539"/>
+        <location filename="curvefont.cpp" line="1880"/>
+        <location filename="curvefont.cpp" line="1950"/>
+        <location filename="curvefont.cpp" line="2020"/>
+        <location filename="curvefont.cpp" line="2090"/>
+        <location filename="curvefont.cpp" line="2160"/>
+        <location filename="curvefont.cpp" line="2230"/>
+        <location filename="curvefont.cpp" line="2300"/>
+        <location filename="curvefont.cpp" line="2370"/>
+        <location filename="curvefont.cpp" line="2440"/>
+        <location filename="curvefont.cpp" line="2510"/>
+        <source>完成</source>
+        <translation>完成</translation>
+    </message>
+    <message>
+        <location filename="curvefont.cpp" line="1547"/>
+        <location filename="curvefont.cpp" line="1588"/>
+        <source>错误</source>
+        <translation>錯誤</translation>
+    </message>
+    <message>
+        <location filename="curvefont.cpp" line="1547"/>
+        <location filename="curvefont.cpp" line="1588"/>
+        <source>当前页面没有数据!</source>
+        <translation>當前頁面沒有數據！</translation>
     </message>
 </context>
 </TS>
